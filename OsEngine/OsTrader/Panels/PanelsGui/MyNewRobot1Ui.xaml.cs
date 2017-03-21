4faa125da257e205ec51155cad24f2987a0d70ee
vovamagik <vovvamagik@mail.ru>
2017-03-21 17:27:43 +0700

    MyNewBot1UI

---
+++ OsEngine/OsTrader/Panels/PanelsGui/MyNewRobot1Ui.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OsEngine.OsTrader.Panels.PanelsGui
+{
+    /// <summary>
+    /// Логика взаимодействия для MyNewRobot1Ui.xaml
+    /// </summary>
+    public partial class MyNewRobot1Ui : Window
+    {
+        private MyNewRobot1 _strategy;
+        public MyNewRobot1Ui(MyNewRobot1 strategy)
+        {
+            InitializeComponent();
+            _strategy = strategy;
+
+            TextBoxVolumeOne.Text = _strategy.VolumeFix.ToString();
+
+            TextBoxSlipage.Text = _strategy.Slipage.ToString(new CultureInfo("ru-RU"));
+
+
+            ComboBoxRegime.Items.Add(BotTradeRegime.Off);
+            ComboBoxRegime.Items.Add(BotTradeRegime.On);
+            ComboBoxRegime.Items.Add(BotTradeRegime.OnlyClosePosition);
+            ComboBoxRegime.Items.Add(BotTradeRegime.OnlyLong);
+            ComboBoxRegime.Items.Add(BotTradeRegime.OnlyShort);
+            ComboBoxRegime.SelectedItem = _strategy.Regime;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+
+                if (Convert.ToInt32(TextBoxVolumeOne.Text) <= 0 ||
+                    Convert.ToDecimal(TextBoxSlipage.Text) < 0)
+                {
+                    throw new Exception("");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("В одном из полей недопустимые значения. Процесс сохранения прерван");
+                return;
+            }
+
+            _strategy.VolumeFix = Convert.ToInt32(TextBoxVolumeOne.Text);
+            _strategy.Slipage = Convert.ToDecimal(TextBoxSlipage.Text);
+
+            Enum.TryParse(ComboBoxRegime.Text, true, out _strategy.Regime);
+
+            Close();
+
+        }
+    }
+}
